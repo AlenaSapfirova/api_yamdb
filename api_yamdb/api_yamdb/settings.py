fb1107d1963cbeec49afdbd7c6c5781df81d20de
Oleg <hans762@yandex.ru>
2023-05-15 10:50:28 +0300

    Модели CGT и их вьюсеты, весия 1.1.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -113,7 +113,7 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
